d559ed3286eaa28d25cb201d60efb0b16ef2efdc
Stefanos Apostolakopoulos <stefanosapost77@gmail.com>
2020-05-06 08:08:59 +0300

    Updates

---
+++ precache-manifest.7a3c9bf7012be98db883db70b386f075.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f776d6889894330c53a9af561903d7e5",
+    "url": "/UdemyProject-RoboFriends/index.html"
+  },
+  {
+    "revision": "978d3e6abce16fc74e6c",
+    "url": "/UdemyProject-RoboFriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "f880ccc095fa771d0629",
+    "url": "/UdemyProject-RoboFriends/static/css/main.5687091a.chunk.css"
+  },
+  {
+    "revision": "978d3e6abce16fc74e6c",
+    "url": "/UdemyProject-RoboFriends/static/js/2.faf57d51.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/UdemyProject-RoboFriends/static/js/2.faf57d51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f880ccc095fa771d0629",
+    "url": "/UdemyProject-RoboFriends/static/js/main.8a4b247d.chunk.js"
+  },
+  {
+    "revision": "678a1f9b3b13e4ed3678",
+    "url": "/UdemyProject-RoboFriends/static/js/runtime-main.8aa5905d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/UdemyProject-RoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
